5cce0b04c155335f01c7f2243685146b103aee1c
victor <victor.murithi@student.moringaschool.com>
2025-01-09 21:36:14 +0300

    added styling on the add task component

--- components/addTask.tsx
+++ components/addTask.tsx
@@ -1,10 +1,25 @@
 import {View, Text, TextInput, TouchableOpacity} from 'react-native'
 import React from 'react'
+import { StyleSheet } from 'react-native'
 
 export default function AddTask() {
     return (
-        <View>
-            <Text>AddTask</Text>
+        <View style={styles.container}>
+            <Text style={styles.text}>AddTask</Text>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#E3F2FD',
+        padding: 20,
+    },
+    text: {
+        fontSize: 20,
+        fontWeight: 'bold',
+    },
+})
